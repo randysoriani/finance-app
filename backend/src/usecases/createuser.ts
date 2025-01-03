e9e5514dd041de2cf86a1f285f5b91a0abc430a4
Randy Soriani <contato@randysoriani.com.br>
2025-01-03 09:18:47 -0300

    refactor: replace static user id to nanoid in create user usecase

--- backend/src/usecases/createuser.ts
+++ backend/src/usecases/createuser.ts
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { IUserRepository } from "../repositories/users";
 
 export class CreateUser{
@@ -7,11 +8,16 @@ export class CreateUser{
         if(!email || !password){return}
 
         const exists = await this.repository.findByEmail(email)
-        console.log(exists)
+        
         if(!exists){
-            const response = await this.repository.save({id: '123', email, password})
+            const user = {
+                id: nanoid(),
+                email,
+                password
+            }
+            const response = await this.repository.save(user)
             if(response){
-                return {id: '132', accessToken: '123', refreshToken: '123'}
+                return {id: user.id, accessToken: '123', refreshToken: '123'}
             }
         }
 
