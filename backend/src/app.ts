e6bee886ec320c8c8838a206b2177413fb51a4e6
Randy Soriani <contato@randysoriani.com.br>
2025-01-03 10:40:22 -0300

    feat: install cors and add to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata'
-import express from 'express'
 import './repositories/index.ts'
+import cors from 'cors'
+import express from 'express'
 
 const server = express()
+server.use(express.json())
+server.use(cors())
 
 server.listen(3000, () => {
     console.log('Server up and running at localhost:3000')
