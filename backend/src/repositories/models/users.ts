244c26b6aa24a78fd8d5a25598d6631bfc964cfb
Randy Soriani <contato@randysoriani.com.br>
2025-01-03 10:42:12 -0300

    feat: create user model and typeorm repository

--- backend/src/repositories/models/users.ts
+++ backend/src/repositories/models/users.ts
@@ -1,7 +1,13 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class UsersModel {
     @PrimaryColumn()
     id!: string
+
+    @Column()
+    email!: string
+
+    @Column()
+    password!: string
 }
---
+++ backend/src/repositories/typeorm/users.ts
@@ -0,0 +1,29 @@
+import { appDataSource } from "..";
+import { User } from "../../entity/user";
+import { UsersModel } from "../models/users";
+import { IUserRepository } from "../users";
+
+export class UserRepositoryORM implements IUserRepository{
+    private repository = appDataSource.getRepository(UsersModel)
+
+    async save(user: User): Promise<boolean> {
+        const response = await this.repository.save({
+            id: String(user.id),
+            email: user.email,
+            password: user.password
+        })
+        if(response){
+            return true
+        }
+
+        return false
+    }
+    
+    async findByEmail(email: string): Promise<User | undefined> {
+        const user = await this.repository.findOneBy({email})
+        if(user){
+            return user
+        }
+        return
+    }
+}
