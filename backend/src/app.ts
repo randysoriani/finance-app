2bd0e314551649e880b3381f813b577be04b2630
Randy Soriani <contato@randysoriani.com.br>
2025-01-03 10:42:31 -0300

    feat: create user router and controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import 'reflect-metadata'
 import './repositories/index.ts'
 import cors from 'cors'
 import express from 'express'
+import { userRouter } from './routes/users.ts'
 
 const server = express()
 server.use(express.json())
 server.use(cors())
+server.use(userRouter)
 
 server.listen(3000, () => {
     console.log('Server up and running at localhost:3000')
---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,31 @@
+import { UserRepositoryORM } from "../repositories/typeorm/users";
+import { CreateUser } from "../usecases/createuser";
+
+export class UsersController{
+    static async create(req: any, res: any){
+        const { email, password} = req.body
+
+        if(!email || !password){
+            return res.status(400).json({status: 'error', message: 'Bad requisition'})
+        }
+
+        const repository = new UserRepositoryORM()
+        const createUser = new CreateUser(repository)
+
+        const result = await createUser.execute(email, password)
+
+        if(result){
+            return res.status(201).json({
+                status: 'ok', 
+                message:'User created', 
+                payload: { 
+                    id: result.id, 
+                    accessToken: result.accessToken, 
+                    refreshToken: result.refreshToken
+                }
+            })
+        }
+
+        return res.status(500).json({status: 'error', message: 'Unable to create new user'})
+    }
+}
