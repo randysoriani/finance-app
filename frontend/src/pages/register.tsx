fe5ce01171208af62a7ce14bb1572babfc04d9d6
Randy Soriani <contato@randysoriani.com.br>
2025-01-07 20:26:31 -0300

    [FE] feat: new user page

---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export function Register(){
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    async function handleFormSubmit(e){
+        e.preventDefault()
+    }
+
+    return(
+        <form onSubmit={handleFormSubmit}>
+            <fieldset>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+            </fieldset>
+
+            <fieldset>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+            </fieldset>
+
+            <button type="submit">Register</button>
+        </form>
+    )
+}
