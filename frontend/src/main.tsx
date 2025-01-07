0b6abadfc82175ceec49db71847d44d3ec32a28f
Randy Soriani <contato@randysoriani.com.br>
2025-01-07 20:27:24 -0300

    [FE] feat: add route to register page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { App } from './App.tsx'
+import { Register } from './pages/register.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
