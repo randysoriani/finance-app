0d42466473fac97f7abb91bf7897ce80dcf7ba08
Randy Soriani <contato@randysoriani.com.br>
2025-01-07 22:22:53 -0300

    [FE] feat: use axios to post register user form

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
+import axios from 'axios'
 
 export function Register(){
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
 
-    async function handleFormSubmit(e){
+    async function handleFormSubmit(e: FormEvent){
         e.preventDefault()
+        const { data, status } = await axios.post('http://localhost:3000/users', {email, password}, {headers: {'Content-Type': 'application/json'}})
+        if(status === 201){
+            console.log('Success: ', data)
+        }
     }
 
     return(
