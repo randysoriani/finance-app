09a5bdc00a322663196d19a5ee13882abbf7dfba
Randy Soriani <contato@randysoriani.com.br>
2025-01-07 22:32:53 -0300

    [FE] feat: create dashboard page and add route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { App } from './App.tsx'
 import { Register } from './pages/register.tsx'
+import { Dashboard } from './pages/dasboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
