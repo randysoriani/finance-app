013333d79cb0d8e3fdea793222059729e429eceb
Randy Soriani <contato@randysoriani.com.br>
2025-01-08 08:35:04 -0300

    [BE] refactor: replace expiration time from string to integer at jwt generator

--- backend/src/helpers/jwt-generators.ts
+++ backend/src/helpers/jwt-generators.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
 
 export function GenerateAccessJWT(payload: {}){
-    return jwt.sign(payload, String(process.env.JWT_SECRET), { expiresIn: '1h' })
+    return jwt.sign(payload, String(process.env.JWT_SECRET), { expiresIn: 60 * 60 })
 }
 
 export function GenerateRefreshJWT(payload: {}){
-    return jwt.sign(payload, String(process.env.JWT_SECRET), { expiresIn: '30d' })
+    return jwt.sign(payload, String(process.env.JWT_SECRET), { expiresIn: 60 * 60 * 24 * 30 })
 }
