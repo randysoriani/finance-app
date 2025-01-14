19217a7980adc8d222c83ebdeb433777ba058048
Randy Soriani <contato@randysoriani.com.br>
2025-01-14 18:36:07 -0300

    [BE] refactor: change account model joins to right types

--- backend/src/repositories/models/account.ts
+++ backend/src/repositories/models/account.ts
@@ -1,7 +1,6 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { UsersModel } from "./users";
 import { InstitutionsModel } from "./institutions";
-import { TransactionsModel } from "./transactions";
 
 @Entity('accounts')
 export class AccountsModel{
@@ -14,21 +13,17 @@ export class AccountsModel{
     @Column()
     description?: string
 
-    @ManyToOne( () => UsersModel, (user) => user.id )
-    @JoinColumn({name: 'user_id'})
-    user_id!: string
-
-    @ManyToOne( () => InstitutionsModel, (inst) => inst.id )
-    @JoinColumn({name: 'institution_id'})
-    institution_id!: string
-
-    @OneToMany(() => TransactionsModel, (transaction: TransactionsModel) => transaction.account_id, {orphanedRowAction: 'delete'})
-    @JoinColumn({name: 'account_id'})
-    transactions!: TransactionsModel[];
-
     @Column()
     agency!: number
 
     @Column()
     account!: number
+
+    @ManyToOne(() => UsersModel, (user) => user.id, {eager: true})
+    @JoinColumn({name: 'user_id'})
+    user_id!: UsersModel
+
+    @ManyToOne(() => InstitutionsModel, (inst) => inst.id, {eager: true})
+    @JoinColumn({name: 'institution_id'})
+    institution_id!: InstitutionsModel
 }
