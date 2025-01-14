768bf09cca302b02ea21d20fbb23fdb36ee0666a
Randy Soriani <contato@randysoriani.com.br>
2025-01-14 18:23:41 -0300

    [BE] refactor: replace user_id and institution_id types in account entity

--- backend/src/entity/account.ts
+++ backend/src/entity/account.ts
@@ -1,8 +1,11 @@
+import { Institution } from "./institution"
+import { User } from "./user"
+
 export interface Account{
     id: string
     description?: string
     agency: number
     account: number
-    user_id: string
-    institution_id: string
+    user_id: User
+    institution_id: Institution
 }
