ad698605ddfe33fe1cafd7365f05282a9c2402b9
Randy Soriani <contato@randysoriani.com.br>
2025-01-14 08:52:28 -0300

    [FE] refactor: replace useState with react-hook-form in login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,23 +1,38 @@
-import { useState } from "react"
+import axios from 'axios'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useNavigate } from "react-router"
+
+export interface IFormData{
+    email: string
+    password: string
+}
 
 export function Login(){
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-
-    async function handleFormSubmit(e){
-        e.preventDefault()
-    }
+    const { handleSubmit, register } = useForm<IFormData>()
     
+    let navigate = useNavigate();
+
+    const onSubmit: SubmitHandler<IFormData> = async (formData) => {
+        const { data, status } = await axios.post('http://localhost:3000/users', formData, {headers: {'Content-Type': 'application/json'}})
+        if(status === 201){
+            const accessToken = data.payload.accessToken
+            const refreshToken = data.payload.refreshToken
+            localStorage.setItem('accessToken', accessToken)
+            localStorage.setItem('refreshToken', refreshToken)
+            navigate("/dashboard");
+        }
+    }
+
     return(
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input type="email" id="email" {...register('email')}/>
             </fieldset>
 
             <fieldset>
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+                <input type="password" id="password" {...register('password')}/>
             </fieldset>
 
             <button type="submit">Login</button>
