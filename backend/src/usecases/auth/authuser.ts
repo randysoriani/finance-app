e42e527e031e4e6769da34f35c5e8089242f4f29
Randy Soriani <contato@randysoriani.com.br>
2025-01-16 07:44:20 -0300

    [BE] refactor: use jwt generator method helper at auth user usecase

--- backend/src/usecases/auth/authuser.ts
+++ backend/src/usecases/auth/authuser.ts
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken'
 import { compareSync } from "bcryptjs";
 import { IUserRepository } from "../../repositories/users";
+import { GenerateAccessJWT, GenerateRefreshJWT } from '../../helpers/jwt-generators';
 
 export class AuthUser{
     constructor(private readonly repository: IUserRepository){}
@@ -16,8 +16,8 @@ export class AuthUser{
             if(compareSync(password, user.password)){
                 return {
                     id: user.id, 
-                    accessToken: jwt.sign({user_id: user.id}, String(process.env.JWT_SECRET), { expiresIn: 60 * 60 }), //1h
-                    refreshToken: jwt.sign({user_id: user.id}, String(process.env.JWT_SECRET), { expiresIn: 60 * 60 * 24 * 30 }) //30d
+                    accessToken: GenerateAccessJWT({user_id: user.id}),
+                    refreshToken: GenerateRefreshJWT({user_id: user.id}) //30d
                 }
             } else {
                 return new BadRequest('Password dont match')
