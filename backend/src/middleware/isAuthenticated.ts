59dbd228bb9ce2b03ccc2c9cd624262b47beb58f
Randy Soriani <contato@randysoriani.com.br>
2025-01-16 07:42:46 -0300

    [BE] fix: change error message in isAuthenticated middleware

--- backend/src/middleware/isAuthenticated.ts
+++ backend/src/middleware/isAuthenticated.ts
@@ -1,4 +1,4 @@
-import { JwtPayload, TokenExpiredError, verify } from "jsonwebtoken";
+import { JwtPayload, verify } from "jsonwebtoken";
 import { NextFunction } from "express";
 
 export async function isAuthenticated(req: any, res: any, next: NextFunction){
@@ -12,7 +12,7 @@ export async function isAuthenticated(req: any, res: any, next: NextFunction){
     try{
         const isValid = verify(token, String(process.env.SECRET)) as JwtPayload;
         if(!isValid.user_id){
-            return res.status(401).json({status:'error', message: 'Token expired'})
+            return res.status(401).json({status:'error', message: 'User not authorized'})
         }
         req.body.user_id = isValid.user_id;
         
