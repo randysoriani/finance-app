fa3d976394412a594e14adabfbe23a2175ef934a
Randy Soriani <contato@randysoriani.com.br>
2025-01-17 22:22:28 -0300

    [FE] style: remove dark style from label component

--- frontend/src/components/Label.tsx
+++ frontend/src/components/Label.tsx
@@ -7,6 +7,6 @@ interface ILabelProps {
 
 export function Label({forField, children}: ILabelProps){
     return(
-        <label htmlFor={forField} className='block text-sm font-medium text-gray-700 dark:text-gray-300'>{children}</label>
+        <label htmlFor={forField} className='block text-sm font-medium text-gray-700 mb-2'>{children}</label>
     )
 }
