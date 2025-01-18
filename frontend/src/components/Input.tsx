cc92bb27413fa6a5b12846f610e836ae614cd240
Randy Soriani <contato@randysoriani.com.br>
2025-01-17 22:21:52 -0300

    [FE] style: add styles to input component

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -3,7 +3,8 @@ import { forwardRef, InputHTMLAttributes } from "react"
 export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
     function Input(props, ref){
         return(
-            <input {...props} ref={ref} />
+            <input {...props} ref={ref} 
+            className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'/>
         )
     }
 )
