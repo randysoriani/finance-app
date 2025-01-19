e93bc98b880058af949647ebd983a5110e9d3c2d
Randy Soriani <contato@randysoriani.com.br>
2025-01-19 11:31:22 -0300

    [FE] feat: change routes to include transactions subpages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,21 +3,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
-import { App } from './App.tsx'
+// import { App } from './App.tsx'
 import { Register } from './pages/register.tsx'
 import { Dashboard } from './pages/dasboard.tsx'
-import { Transactions } from './pages/transactions.tsx'
+import { Transactions } from './pages/transactions/index.tsx'
 import { Login } from './pages/login.tsx'
+import { Main } from './pages/main.tsx'
+import { Accounts } from './pages/transactions/accounts.tsx'
+import { Summary } from './pages/transactions/summary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        {/* <Route path='/' element={<App />} /> */}
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/transactions' element={<Transactions />} />
+        <Route path='/' element={<Main />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/transactions' element={<Transactions />}>
+            <Route index={true} element={<Summary />} />
+            <Route path='/transactions/accounts' element={<Accounts />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
