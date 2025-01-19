aaa7bea239a066af218e8e4581feee4a5eb63d2f
Randy Soriani <contato@randysoriani.com.br>
2025-01-19 11:30:54 -0300

    [FE] refactor: remove sidemenu from dashboard

--- frontend/src/pages/dasboard.tsx
+++ frontend/src/pages/dasboard.tsx
@@ -1,9 +1,6 @@
-import { Sidemenu } from "../components/Sidemenu";
-
 export function Dashboard(){
     return(
         <div>
-            <Sidemenu />
             <main>
                 <h2>Dashboard</h2>
             </main>
