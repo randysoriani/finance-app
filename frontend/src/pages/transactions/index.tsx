b5c235f03622e6ed139e3527e9f90bf1a141d421
Randy Soriani <contato@randysoriani.com.br>
2025-01-22 08:55:23 -0300

    [FE] style: add style to transactions submenu

--- frontend/src/pages/transactions/index.tsx
+++ frontend/src/pages/transactions/index.tsx
@@ -1,17 +1,25 @@
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
+import { SectionHeader } from "../../components/SectionHeader";
+import { GrTransaction } from "react-icons/gr";
 
 export function Transactions(){
     return(
         <div>
-            <main>
-                <h2>Transactions</h2>
-                <nav>
-                    <Link to='/transactions'>General</Link>
-                    <Link to='/transactions/accounts'>Accounts</Link>
-                </nav>
-                
-                <Outlet />
-            </main>
+            <SectionHeader title="Transactions" subtitle="Keep track of incomes and outcomes" icon={<GrTransaction size={38} />} />
+            <nav className='mb-8 mt-4 flex gap-4'>
+                <NavLink end={true} className={({isActive}) => `
+                            pb-2
+                            ${isActive 
+                                ? 'font-semibold border-b-2 border-gray-800' : 'font-normal border-none' }`} 
+                            to='/transactions'>General</NavLink>
+                <NavLink className={({isActive}) => `
+                            pb-2
+                            ${isActive 
+                                ? 'font-semibold border-b-2 border-gray-800' : 'font-normal border-none'}`} 
+                            to='/transactions/accounts'>Accounts</NavLink>
+            </nav>
+            
+            <Outlet />
         </div>
     )
 }
