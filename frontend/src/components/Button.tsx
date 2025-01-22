94e3ef224fc28845d34bd8d28471834a366e0f6c
Randy Soriani <contato@randysoriani.com.br>
2025-01-22 08:53:02 -0300

    [FE] feat: add styles to Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,13 +1,23 @@
 import { ButtonHTMLAttributes } from "react"
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    onClick?: () => {}
+    onClick?: () => void
+    kind?: 'Primary' | 'Secondary' | 'Ghost' | 'Link' | 'Icon'
     children: React.ReactNode
 }
 
-export function Button({onClick, children}: IProps){
+export function Button({onClick, children, kind = 'Secondary'}: IProps){
     return(
-        <button onClick={onClick}>
+        <button onClick={onClick}
+                className={
+                    `   rounded px-4 py-2 flex items-center justify-center
+                        ${kind === 'Primary' && 'bg-indigo-500'}
+                        ${kind === 'Secondary' && 'bg-gray-300'}
+                        ${kind === 'Ghost' && 'bg-transparent border-gray-500 border-[1px]'}
+                        ${kind === 'Link' && 'bg-transparent'}
+                        ${kind === 'Icon' && 'bg-transparent p-0'}
+                    `
+                }>
             {children}
         </button>
     )
