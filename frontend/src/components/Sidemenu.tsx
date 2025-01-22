49a62161f253a54ebb2eca22c5ac76af9ba4195d
Randy Soriani <contato@randysoriani.com.br>
2025-01-22 08:54:07 -0300

    [FE] style: add style for sidemenu

--- frontend/src/components/Sidemenu.tsx
+++ frontend/src/components/Sidemenu.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router";
+import { GrTransaction } from "react-icons/gr";
+import { SlGraph } from "react-icons/sl";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { NavLink } from "react-router";
 
 export function Sidemenu(){
     return(
-        <aside>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/transactions">Transactions</Link>
+        <aside className='bg-white flex gap-1 flex-col min-w-52'>
+            <div className='mb-24' />
+
+            <NavLink className={({isActive}) => `text-gray-500 w-full py-1 px-4 hover:text-gray-800 flex gap-2 items-center
+                            ${isActive && 'text-white bg-sky-600 hover:text-white' }`}  
+                      to="/dashboard">
+                <TbLayoutDashboard />
+                <span>Dashboard</span>
+            </NavLink>
+
+            <NavLink className={({isActive}) => `text-gray-500 w-full py-1 px-4 hover:text-gray-800 flex gap-2 items-center
+                            ${isActive && 'text-white bg-sky-600 hover:text-white' }`} 
+                      to="/transactions">
+                <GrTransaction />
+                <span>Transactions</span>
+            </NavLink>
+
+            <NavLink className={({isActive}) => `text-gray-500 w-full py-1 px-4 hover:text-gray-800 flex gap-2 items-center
+                            ${isActive && 'text-white bg-sky-600 hover:text-white' }`}
+                      to="/investments">
+                <SlGraph />
+                <span>Investments</span>
+            </NavLink>
         </aside>
     )
 }
